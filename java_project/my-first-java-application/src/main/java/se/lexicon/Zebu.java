b9cb9e17798d7111adab4541529116db65ef0ff4
JEAN-PIERRE <jeanpierre.salum@outlook.com>
2024-10-07 09:53:54 +0200

    first commit on Java

---
+++ java_project/my-first-java-application/src/main/java/se/lexicon/Zebu.java
@@ -0,0 +1,12 @@
+package se.lexicon;
+
+public class Zebu {
+    public static void main(String[] args) {
+
+        int score1 = 100;
+        int score2 = 200;
+        int finalScore = score1 + score2;
+
+        System.out.println(finalScore);
+    }
+}
